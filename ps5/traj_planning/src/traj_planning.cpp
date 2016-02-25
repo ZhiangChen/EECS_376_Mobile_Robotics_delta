84bdf9b4ca262909fd79db90c25f6a21e8a5218f
ZhiangChen <zxc251@case.edu>
2016-02-25 16:23:10 -0500

    ps5

--- ps5/traj_planning/src/traj_planning.cpp
+++ ps5/traj_planning/src/traj_planning.cpp
@@ -1,4 +1,7 @@
 #include <pub_des_state/ps5_pub_des_state.h>
+#include <traj_planning/subgoals_msg.h>
+
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "traj_planning");
     ros::NodeHandle nh;
@@ -15,16 +18,17 @@ int main(int argc, char **argv) {
     desStatePublisher.append_path_queue(0.0,5.0,0.0);
     desStatePublisher.append_path_queue(0.0,0.0,0.0);
     
+    std::vector<nav_msgs::Odometry> subgoals;
     // main loop; publish a desired state every iteration
     while (ros::ok()) {
         desStatePublisher.pub_next_state();
-	if(desStatePublisher.get_estop_trigger())
-	{
-		ROS_INFO("estop is triggered");
-	}
-        ros::spinOnce();
-        looprate.sleep(); //sleep for defined sample period, then do loop again
-    }
+    	if(desStatePublisher.get_estop_trigger())
+    	{
+    		ROS_INFO("estop is triggered");
+    	}
+            ros::spinOnce();
+            looprate.sleep(); //sleep for defined sample period, then do loop again
+        }
 }
 
 
