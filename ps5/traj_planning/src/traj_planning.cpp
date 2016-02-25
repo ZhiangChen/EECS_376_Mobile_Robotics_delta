b517d473dcfeb03b2d587c09227a79c83351f7e3
ZhiangChen <zxc251@case.edu>
2016-02-25 14:27:30 -0500

    lidar

--- ps5/traj_planning/src/traj_planning.cpp
+++ ps5/traj_planning/src/traj_planning.cpp
@@ -8,14 +8,23 @@ int main(int argc, char **argv) {
     ros::Rate looprate(1 / dt); //timer for fixed publication rate
     desStatePublisher.set_init_pose(0,0,0); //x=0, y=0, psi=0
     //put some points in the path queue--hard coded here
+
+
     desStatePublisher.append_path_queue(5.0,0.0,0.0);
+    desStatePublisher.append_path_queue(5.0,5.0,0.0);
+    desStatePublisher.append_path_queue(0.0,5.0,0.0);
     desStatePublisher.append_path_queue(0.0,0.0,0.0);
     
     // main loop; publish a desired state every iteration
     while (ros::ok()) {
         desStatePublisher.pub_next_state();
+	if(desStatePublisher.get_estop_trigger())
+	{
+		ROS_INFO("estop is triggered");
+	}
         ros::spinOnce();
         looprate.sleep(); //sleep for defined sample period, then do loop again
     }
 }
+
 
