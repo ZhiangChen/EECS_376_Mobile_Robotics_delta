d80eef3a760f0aa9af38f95c67e7fa860cd4819b
ECassak <edc39@case.edu>
2016-04-20 11:08:57 -0400

    Process Frame

--- final_project/process_frame/src/process_frame.cpp
+++ final_project/process_frame/src/process_frame.cpp
@@ -10,7 +10,63 @@ int main(int argc, char **argv) {
     CanSearcher Cansearcher(&nh);
     MoveJinx Movejinx(&nh);
     MoveArm Movearm(&nh);
+    Eigen:Vector3f centroid;
+    bool got_table = false;
+    bool got_can = false;
 
+    Movearm.setPrepose();
+    Movejinx.move2LT();
+    Cansearcher.takeAPic();
+    got_table = Cansearcher.searchTable();
+    if (got_table)
+    {
+    	got_can = Cansearcher.searchCan2(centroid);
+    }
+    else
+    {
+    	ROS_ERROR("Failed to find the left table.");
+    	return 0;
+    }
+    if (got_can)
+    {
+    	ROS_INFO("Got the can on the left table. Picking it up.");
+    		Movearm.graspCan(centroid);
+		Movearm.moveCan();
+		Movearm.dropcan();
+		return 0;
+    }
+    else
+    {
+    	ROS_INFO("Cannot find a coke can on the left table.");
+    	ROS_INFO("Moving to the right table...");
+    	Movejinx.moveBack();
+    	Cansearcher.takeAPic();
+    	got_table = false;
+    	got_table = Cansearcher.searchTable();
+    	if (got_table)
+    	{
+    		got_can = false;
+    		got_can = Cansearcher.searchCan2(centroid);
+    	}
+    	else
+    	{
+	     	ROS_ERROR("Failed to find the right table.");
+	    	return 0;   		
+    	}
+    	if(got_can)
+    	{
+	    	ROS_INFO("Got the can on the right table. Picking it up.");
+	    	Movearm.graspCan(centroid);
+		Movearm.moveCan();
+		Movearm.dropcan();
+		return 0;
+    	}
+    	else
+    	{
+    		ROS_INFO("Cannot find a coke can on the right table.");
+    		return 0;
+    	}
+    }
 
 }
 
